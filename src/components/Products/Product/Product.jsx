ff391e6e4bd8c04d5e9a278372100946c4332d6c
mspcity <codepark2019@gmail.com>
2021-07-26 00:05:32 -0400

    components added

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core'
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons'
 
 const Product = () => {
   return (
