c56e9d50ad55d487838e12ba9ebcd9e877c414cb
mspcity <codepark2019@gmail.com>
2021-07-27 12:39:40 -0400

    Typography

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons'
 
-const Product = () => {
+const Product = ({ product }) => {
   return (
     <div>
         <Card className={classes.root}>
-            <CardMedia className={classes.media} />
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.CardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+                </div>
+            </CardContent>
         </Card>
     </div>
   )
