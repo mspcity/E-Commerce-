8509f108587db0e2da1eaea5fbae5bc357318954
mspcity <codepark2019@gmail.com>
2021-07-27 15:04:56 -0400

    created styles.js, import useStyles from styles.js

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons'
 
+import useStyles from './styles';
+
 const Product = ({ product }) => {
+  const classes = useStyles();
+  
   return (
     <div>
         <Card className={classes.root}>
