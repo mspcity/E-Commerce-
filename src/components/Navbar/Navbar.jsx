fbb6996b83855c41269176034e770f3311e11201
mspcity <codepark2019@gmail.com>
2021-07-28 22:23:16 -0400

    working on Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import { classExpression } from '@babel/types';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+        <AppBar position="fixed" className={classes.appBar} color="inherit">
+            <Toolbar variant="h6" className={classes.title} color="inherit">
+                <Typography>
+                    <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+                    Commerce.js
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    </>
   )
 }
 
