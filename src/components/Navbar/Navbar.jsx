a3b83e57fe70a178295eb603a91dd663f38148c4
mspcity <codepark2019@gmail.com>
2021-08-04 00:26:38 -0400

    Navbar Typography edied

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <>
         <AppBar position="fixed" className={classes.appBar} color="inherit">
             <Toolbar variant="h6" className={classes.title} color="inherit">
-                <Typography>
+                <Typography variant="h6" className={classes.title} color="inherit">
                     <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
                     Park's Store
                 </Typography>
