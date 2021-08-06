b1dfe7ca7c7bf30c802ded55a3ed3ebf5e9e5fd6
mspcity <codepark2019@gmail.com>
2021-08-05 22:18:01 -0400

    tatalItems props added on App.js and passing them to Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { ShoppingCart } from '@material-ui/icons';
 import logo from '../../assets/commerce.png'
 import useStyles from './styles';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
   const classes = useStyles();
 
   return (
