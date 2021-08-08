7563db642ec674344b0e327e9c57b93e7a40101f
mspcity <codepark2019@gmail.com>
2021-08-07 22:21:17 -0400

    Container

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,9 +3,10 @@ import { Container, Typography, Button, Grid } from '@material-ui/core'
 
 const Cart = () => {
   return (
-    <div>
-      
-    </div>
+      <Container>
+          <div className={classes.tooblar} />
+          
+      </Container>
   )
 }
 
