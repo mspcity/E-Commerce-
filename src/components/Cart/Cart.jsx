9f3cbc309404a7e33124d3c65ee2d8606af20f39
mspcity <codepark2019@gmail.com>
2021-08-08 20:18:56 -0400

    created Empty Cart and FilledCart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
 
 const Cart = () => {
+  const isEmpty = true;
+
+  const EmptyCart = () => (
+      <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+  );
+
+  const FilledCart = () => (
+      <>
+      <Grid container spacing={3}>
+          
+      </Grid>
+      </>
+  );
+
   return (
       <Container>
           <div className={classes.tooblar} />
-          
+          <Typography className={classes.title} variant="h3">Your Shopping Car</Typography>
+          { ifEmpty ? <EmptyCart /> : <FilledCart />}
       </Container>
   )
 }
